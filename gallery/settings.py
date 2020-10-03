70760552c0c74ce6775baed1afaa8787e2f6a646
Christine <teenahshyro@gmail.com>
2020-10-03 22:12:37 +0300

    Changed the user.

--- gallery/settings.py
+++ gallery/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gallery',
-        'USER': "postgres",
+        'USER': "christine",
         'PASSWORD':"christine"
     }
 }
